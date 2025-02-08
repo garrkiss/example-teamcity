04a1e825683a5c19b3dcf70c3d111ee73022326b
Evgenii.Bakulev <garks91@mail.ru>
2025-02-08 14:45:48 +0300

    Add new method

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,16 +1,34 @@
 package plaindoll;
 
-public class Welcomer{
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
+import java.util.Random;
+
+public class Welcomer {
+
+    private final String[] hunterReplies = {
+        "The path of the hunter is never easy.",
+        "Every hunter has a story worth telling.",
+        "Even the strongest hunter must rest sometimes."
+    };
+
+    public String sayWelcome() {
+        return "Welcome home, good hunter. What is it your desire?";
+    }
+
+    public String sayFarewell() {
+        return "Farewell, good hunter. May you find your worth in waking world.";
+    }
+
+    public String sayNeedGold() {
+        return "Not enough gold";
+    }
+
+    public String saySome() {
+        return "Something in the way";
+    }
+
+    public String sayHunterReply() {
+        Random random = new Random();
+        int index = random.nextInt(hunterReplies.length);
+        return hunterReplies[index];
+    }
 }
--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -6,28 +6,38 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WelcomerTest {
-	
-	private Welcomer welcomer = new Welcomer();
 
-	@Test
-	public void welcomerSaysWelcome() {
-		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
-	}
-	@Test
-	public void welcomerSaysFarewell() {
-		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
-	}
-	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
-	@Test
-	public void welcomerSaysSilver(){
-		assertThat(welcomer.sayNeedGold(), containsString("gold"));
-	}
-	@Test
-	public void welcomerSaysSomething(){
-		assertThat(welcomer.saySome(), containsString("something"));
-	}
+    private Welcomer welcomer = new Welcomer();
+
+    @Test
+    public void welcomerSaysWelcome() {
+        assertThat(welcomer.sayWelcome(), containsString("Welcome"));
+    }
+
+    @Test
+    public void welcomerSaysFarewell() {
+        assertThat(welcomer.sayFarewell(), containsString("Farewell"));
+    }
+
+    @Test
+    public void welcomerSaysHunter() {
+        assertThat(welcomer.sayWelcome(), containsString("hunter"));
+        assertThat(welcomer.sayFarewell(), containsString("hunter"));
+    }
+
+    @Test
+    public void welcomerSaysSilver() {
+        assertThat(welcomer.sayNeedGold(), containsString("gold"));
+    }
+
+    @Test
+    public void welcomerSaysSomething() {
+        assertThat(welcomer.saySome(), containsString("something"));
+    }
+
+    @Test
+    public void welcomerSaysHunterReplyContainsHunter() {
+        String reply = welcomer.sayHunterReply();
+        assertThat("Reply should contain the word 'hunter'", reply, containsString("hunter"));
+    }
 }
